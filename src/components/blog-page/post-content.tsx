4833d4d7b990b8a5ebdc092ea8bb581db1d24b8b
cakes <cakes@haiiro.moe>
2025-05-12 21:59:56 +0200

    feat(blog): add markdown rendering and a skeleton loader

--- src/components/blog-page/post-content.tsx
+++ src/components/blog-page/post-content.tsx
@@ -1,6 +1,10 @@
 "use client";
 import useSWR from "swr";
 import { Post } from "./get-posts";
+import Markdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import rehypeRaw from "rehype-raw";
 
 function fetcher(
 	url: string,
@@ -45,9 +49,28 @@ export default function PostContent({ slug }: { slug: string }) {
 
 	if (isLoading) {
 		return (
-			<div className="flex justify-center items-center min-h-[90vh] font-serif text-5xl">
-				TODO: Add a skeleton page here.
-			</div>
+			<>
+				<section className="flex flex-col border-b border-base-300 w-full blob">
+					<div className="mx-auto mt-48 pb-16 container">
+						<h1 className="font-serif font-extralight text-8xl">
+							Loading post...
+						</h1>
+						<div className="mx-3">
+							<p className="mt-4 max-w-lg text-2xl">
+								Please wait while we fetch the post content.
+							</p>
+						</div>
+					</div>
+				</section>
+				<section className="flex flex-col bg-base-100 border-b border-base-300 w-full">
+					<div className="relative flex flex-col mx-auto mt-16 pb-16 text-left container">
+						<div className="pb-52 post-content">
+							There should be a post here, but unfortunately, it
+							is loading. Please wait a moment.
+						</div>
+					</div>
+				</section>
+			</>
 		);
 	}
 
@@ -76,6 +99,28 @@ export default function PostContent({ slug }: { slug: string }) {
 						<p className="mt-4 max-w-lg text-2xl">
 							{response && response[0] && response[0].description}
 						</p>
+						<p>
+							{response &&
+								response[0] &&
+								new Date(response[0].date).toLocaleDateString(
+									"en-US",
+									{
+										year: "numeric",
+										month: "long",
+										day: "numeric",
+									}
+								)}
+							{response[0].tags &&
+								response[0].tags.length > 0 && (
+									<span className="text-gray-500 text-sm">
+										{" "}
+										-{" "}
+										{response[0].tags
+											.map((tag) => `#${tag}`)
+											.join(", ")}
+									</span>
+								)}
+						</p>
 					</div>
 				</div>
 			</section>
@@ -84,8 +129,50 @@ export default function PostContent({ slug }: { slug: string }) {
 					{/* TODO: Filtering */}
 					{response && response[0] && (
 						<div className="post-content">
-							{/* TODO: Parse markdown!!! */}
-							{response[0].content}
+							<Markdown
+								remarkPlugins={[remarkMath]}
+								rehypePlugins={[rehypeKatex, rehypeRaw]}
+								components={{
+									p: ({ node, ...props }) => (
+										<p
+											className="my-4 text-md"
+											{...props}
+										/>
+									),
+									h1: ({ node, ...props }) => (
+										<h1
+											className="my-4 font-serif text-5xl"
+											{...props}
+										/>
+									),
+									h2: ({ node, ...props }) => (
+										<h2
+											className="my-4 font-serif text-4xl"
+											{...props}
+										/>
+									),
+									h3: ({ node, ...props }) => (
+										<h3
+											className="my-4 text-3xl"
+											{...props}
+										/>
+									),
+									h4: ({ node, ...props }) => (
+										<h4
+											className="my-4 text-2xl"
+											{...props}
+										/>
+									),
+									h5: ({ node, ...props }) => (
+										<h5
+											className="my-4 text-xl"
+											{...props}
+										/>
+									),
+								}}
+							>
+								{response[0].content}
+							</Markdown>
 						</div>
 					)}
 				</div>
