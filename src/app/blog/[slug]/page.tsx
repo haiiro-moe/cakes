eb91a20dbd0435ad64688bdbd94854c8e19aad85
cakes <cakes@haiiro.moe>
2025-05-14 00:37:57 +0200

    feat(blog): metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,74 @@
+import type { Post } from "@/components/blog-page/get-posts";
 import PostContent from "@/components/blog-page/post-content";
 import RandomQuote from "@/components/home-page/random-quote";
+import type { Metadata, ResolvingMetadata } from "next";
 
-export default async function BlogPostPage({
-	params,
-}: {
+type Props = {
 	params: Promise<{ slug: string }>;
-}) {
+};
+
+export async function generateMetadata(
+	{ params }: Props,
+	parent: ResolvingMetadata
+): Promise<Metadata> {
+	const slug = (await params).slug;
+	const parentMeta = await parent;
+
+	// fetch post information
+	// TODO: LOCALHOST HERE. PLEASE PLEASE PLEASE DONT FORGET ABOUT THIS.
+	const post = await fetch(`http://localhost:3001/api/blog/posts`)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Failed to fetch data");
+			}
+			if (res.status === 404) {
+				throw new Error("No data found");
+			}
+			if (res.status === 500) {
+				throw new Error("Server error");
+			}
+			return res.json();
+		})
+		.then((posts) => {
+			const post = posts.find((p: Post) => p.slug === slug);
+			if (!post) {
+				throw new Error("No post found");
+			}
+			return post;
+		});
+
+	return {
+		...(parentMeta as Metadata),
+		title: `~cakes - ${post.title}`,
+		description: post.description,
+		openGraph: {
+			title: post.title,
+			description: post.description,
+			url: `https://haiiro.moe/~cakes/blog/${slug}`,
+			images: [
+				{
+					url: post.image,
+					width: 1200,
+					height: 630,
+				},
+			],
+		},
+		twitter: {
+			card: "summary_large_image",
+			title: post.title,
+			description: post.description,
+			images: [
+				{
+					url: post.image,
+					width: 1200,
+					height: 630,
+				},
+			],
+		},
+	};
+}
+
+export default async function BlogPostPage({ params }: Props) {
 	const { slug } = await params;
 
 	return (
