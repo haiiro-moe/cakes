45da7648a7cf001373cd44bb42cd90ac8d6038b4
cakes <cakes@haiiro.moe>
2025-05-18 16:43:30 +0200

    feat(home): replace Link with RouterLink for improved navigation and update project descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Container from "@/components/container";
 import OnlineDisplay from "@/components/home-page/online-display";
 import ProjectCards from "@/components/home-page/project-cards";
 import RandomQuote from "@/components/home-page/random-quote";
+import RouterLink from "@/components/router-link";
 import { Endpoints } from "@octokit/types";
 import type { Metadata, ResolvingMetadata } from "next";
-import Link from "next/link";
 
 const projectCache: {
 	[key: string]: {
@@ -69,30 +69,48 @@ export default async function Home() {
 						A personal site for sharing my thoughts, ideas and
 						projects. I&apos;m cakes. Nice to meet you!
 					</p>
-					<Link href="/about" className="mt-6 btn btn-primary">
+					<RouterLink href="/about" className="mt-6 btn btn-primary">
 						Tell me more!
-					</Link>
+					</RouterLink>
 				</div>
 			</Container>
 			<Container asSection textAlign="center">
 				<h1 className="font-semibold text-6xl">Some of my projects</h1>
 				<p className="mt-4 text-2xl">
-					Here are some of my projects that I have worked on.
+					Here are my most recently updated projects.
 				</p>
 				<ProjectCards projects={projects} />
-				<div className="text-center">
-					<Link href="/projects" className="mt-6 btn btn-primary">
-						See all my projects
-					</Link>
+				<div className="flex justify-center gap-4 text-center">
+					<RouterLink
+						href="/projects"
+						className="mt-6 btn btn-primary"
+					>
+						See my notable projects
+					</RouterLink>
+					<RouterLink
+						href="https://github.com/Kex1016?tab=repositories"
+						className="mt-6 btn btn-secondary"
+						target="_blank"
+					>
+						<svg
+							role="img"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+							className="fill-current w-5 h-5"
+						>
+							<title>GitHub</title>
+							<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+						</svg>
+					</RouterLink>
 				</div>
 			</Container>
 			<Container asSection bgVariant>
 				<h1 className="font-semibold text-6xl">About me</h1>
 				<p className="mt-4 text-2xl">
 					I have an{" "}
-					<Link href="/about" className="link link-primary">
+					<RouterLink href="/about" className="link link-primary">
 						about page
-					</Link>{" "}
+					</RouterLink>{" "}
 					if you want to know even more about me, but here are some
 					tidbits:
 				</p>
