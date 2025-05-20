d1d4eef345f02ea33d390a06a51fc26043cd313a
cakes <cakes@haiiro.moe>
2025-05-20 21:09:17 +0200

    feat(blog): add metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,28 +3,21 @@ import Container from "@/components/container";
 import RandomQuote from "@/components/home-page/random-quote";
 import { Metadata } from "next";
 
-// TODO: Move stuff to properly separate client and server components
-// export const metadata: Metadata = {
-// 	title: "Blog",
-// 	description: "My blog posts",
-// 	openGraph: {
-// 		title: "Blog",
-// 		description: "My blog posts",
-// 		url: "https://haiiro.moe/~cakes/blog",
-// 	},
-// 	twitter: {
-// 		card: "summary_large_image",
-// 		title: "Blog",
-// 		description: "My blog posts",
-// 		site: "https://haiiro.moe/~cakes/blog",
-// 		images: [
-// 			{
-// 				url: "https://haiiro.moe/~cakes/blog/og-image.png",
-// 				alt: "Blog",
-// 			},
-// 		],
-// 	},
-// };
+export const metadata: Metadata = {
+	title: "~cakes - blog",
+	description: "My blog posts",
+	openGraph: {
+		title: "~cakes - blog",
+		description: "My blog posts",
+		url: "https://haiiro.moe/~cakes/blog",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "~cakes - blog",
+		description: "My blog posts",
+		site: "https://haiiro.moe/~cakes/blog",
+	},
+};
 
 export default function BlogPage() {
 	return (
